e774e56cb653c02f17e639da33ef407fff531be4
Youtao Xing <youtao.xing@icloud.com>
2020-08-03 12:51:56 +0800

    create package: fisco

---
+++ packages/fisco/lib/index.js
@@ -0,0 +1,36 @@
+/*
+ This file is part of crypu.js.
+
+ crypu.js is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Lesser General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ crypu.js is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public License
+ along with crypu.js.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/**
+ * @file index.ts
+ * @author Youtao Xing <youtao.xing@icloud.com>
+ * @date 2020
+ */
+'use strict';
+Object.defineProperty(exports, "__esModule", { value: true });
+var signing_key_1 = require("@ethersproject/signing-key");
+Object.defineProperty(exports, "SigningKey", { enumerable: true, get: function () { return signing_key_1.SigningKey; } });
+var abi_1 = require("@crypujs/abi");
+Object.defineProperty(exports, "Fragment", { enumerable: true, get: function () { return abi_1.Fragment; } });
+Object.defineProperty(exports, "FunctionFragment", { enumerable: true, get: function () { return abi_1.FunctionFragment; } });
+Object.defineProperty(exports, "EventFragment", { enumerable: true, get: function () { return abi_1.EventFragment; } });
+Object.defineProperty(exports, "Interface", { enumerable: true, get: function () { return abi_1.Interface; } });
+var abstract_provider_1 = require("@crypujs/abstract-provider");
+Object.defineProperty(exports, "Provider", { enumerable: true, get: function () { return abstract_provider_1.Provider; } });
+var providers_1 = require("@crypujs/providers");
+Object.defineProperty(exports, "JsonRpcProvider", { enumerable: true, get: function () { return providers_1.JsonRpcProvider; } });
+var wallet_1 = require("@crypujs/wallet");
+Object.defineProperty(exports, "Wallet", { enumerable: true, get: function () { return wallet_1.Wallet; } });
