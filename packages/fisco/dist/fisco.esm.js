fab7f84914ef38f8b915986585ad933ca3443b07
Youtao Xing <youtao.xing@icloud.com>
2020-08-11 09:48:22 +0800

    support call

--- packages/fisco/dist/fisco.esm.js
+++ packages/fisco/dist/fisco.esm.js
@@ -14193,6 +14193,8 @@ class JsonRpcProvider extends BaseProvider {
                 return ['getTransactionByHash', [this.groupId, params.transactionHash]];
             case 'getTransactionReceipt':
                 return ['getTransactionReceipt', [this.groupId, params.transactionHash]];
+            case 'call':
+                return ['call', [this.groupId, params.transaction]];
             default:
                 logger$l.throwError(method + ' not implemented', Logger.errors.NOT_IMPLEMENTED, { operation: method });
         }
