0103052fd7e2854c1aadc4c0ca623de95218c356
Youtao Xing <youtao.xing@icloud.com>
2020-08-28 14:20:35 +0800

    refactor(providers): unify getChainId and prepareRequest

--- packages/crypu/dist/crypu.esm.js
+++ packages/crypu/dist/crypu.esm.js
@@ -14081,147 +14081,6 @@ function poll(func, options) {
     });
 }
 
-/*
- This file is part of crypu.js.
-
- crypu.js is free software: you can redistribute it and/or modify
- it under the terms of the GNU Lesser General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
-
- crypu.js is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU Lesser General Public License for more details.
-
- You should have received a copy of the GNU Lesser General Public License
- along with crypu.js.  If not, see <http://www.gnu.org/licenses/>.
- */
-/**
- * @file ethers.api.ts
- * @author Youtao Xing <youtao.xing@icloud.com>
- * @date 2020
- */
-const Api = {
-    detectChainId: (send) => {
-        return () => send('eth_chainId', []);
-    },
-    prepareRequest: (method, params) => {
-        switch (method) {
-            case 'getBlockNumber':
-                return ['eth_blockNumber', []];
-            case 'getGasPrice':
-                return ['eth_gasPrice', []];
-            case 'getBalance':
-                return ['eth_getBalance', [params.address.toLowerCase(), params.blockTag]];
-            case 'getTransactionCount':
-                return ['eth_getTransactionCount', [params.address.toLowerCase(), params.blockTag]];
-            case 'getCode':
-                return ['eth_getCode', [params.address.toLowerCase(), params.blockTag]];
-            case 'getStorageAt':
-                return ['eth_getStorageAt', [params.address.toLowerCase(), params.position, params.blockTag]];
-            case 'sendTransaction':
-                return ['eth_sendRawTransaction', [params.signedTransaction]];
-            case 'getBlock':
-                if (params.blockTag) {
-                    return ['eth_getBlockByNumber', [params.blockTag, !!params.includeTransactions]];
-                }
-                else if (params.blockHash) {
-                    return ['eth_getBlockByHash', [params.blockHash, !!params.includeTransactions]];
-                }
-                return null;
-            case 'getTransaction':
-                return ['eth_getTransactionByHash', [params.transactionHash]];
-            case 'getTransactionReceipt':
-                return ['eth_getTransactionReceipt', [params.transactionHash]];
-            case 'call': {
-                return ['eth_call', [params.transaction, params.blockTag]];
-            }
-            case 'estimateGas': {
-                return ['eth_estimateGas', [params.transaction]];
-            }
-            case 'getLogs':
-                if (params.filter && params.filter.address != null) {
-                    params.filter.address = params.filter.address.toLowerCase();
-                }
-                return ['eth_getLogs', [params.filter]];
-            default:
-                break;
-        }
-        return null;
-    },
-};
-
-/*
- This file is part of crypu.js.
-
- crypu.js is free software: you can redistribute it and/or modify
- it under the terms of the GNU Lesser General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
-
- crypu.js is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU Lesser General Public License for more details.
-
- You should have received a copy of the GNU Lesser General Public License
- along with crypu.js.  If not, see <http://www.gnu.org/licenses/>.
- */
-/**
- * @file fisco.api.ts
- * @author Youtao Xing <youtao.xing@icloud.com>
- * @date 2020
- */
-var Api$1 = {
-    detectChainId: (send) => {
-        return () => send('getClientVersion', []).then((clientVersion) => Number(clientVersion['Chain Id']));
-    },
-    prepareRequest: (groupId) => {
-        return (method, params) => {
-            switch (method) {
-                case 'getClientVersion':
-                    return ['getClientVersion', []];
-                case 'getPbftView':
-                    return ['getPbftView', [groupId]];
-                case 'getSealerList':
-                    return ['getSealerList', [groupId]];
-                case 'getObserverList':
-                    return ['getObserverList', [groupId]];
-                case 'getSyncStatus':
-                    return ['getSyncStatus', [groupId]];
-                case 'getPeers':
-                    return ['getPeers', [groupId]];
-                case 'getNodeIdList':
-                    return ['getNodeIDList', [groupId]];
-                case 'getGroupList':
-                    return ['getGroupList', [groupId]];
-                case 'getBlockNumber':
-                    return ['getBlockNumber', [groupId]];
-                case 'getBlock':
-                    if (params.blockTag) {
-                        return ['getBlockByNumber', [groupId, params.blockTag, !!params.includeTransactions]];
-                    }
-                    else if (params.blockHash) {
-                        return ['getBlockByHash', [groupId, params.blockHash, !!params.includeTransactions]];
-                    }
-                    break;
-                case 'sendTransaction':
-                    return ['sendRawTransaction', [groupId, params.signedTransaction]];
-                case 'getTransaction':
-                    return ['getTransactionByHash', [groupId, params.transactionHash]];
-                case 'getTransactionReceipt':
-                    return ['getTransactionReceipt', [groupId, params.transactionHash]];
-                case 'call':
-                    return ['call', [groupId, params.transaction]];
-                default:
-                    break;
-            }
-            return null;
-        };
-    },
-};
-
 "use strict";
 function keccak256$3(data) {
     return '0x' + sha3.keccak_256(arrayify$6(data));
@@ -20333,7 +20192,7 @@ const logger$t = new Logger('provider');
 const defaultUrl = 'http://localhost:8545';
 const defaultNetwork = {
     chainId: 1,
-    name: 'fisco',
+    name: 'ethers',
 };
 let defaultFormatter$1;
 class JsonRpcProvider extends BaseProvider {
@@ -20344,25 +20203,9 @@ class JsonRpcProvider extends BaseProvider {
             url = getStatic((new.target), 'defaultUrl')();
         }
         defineReadOnly(this, 'connection', { url: url });
-        switch (chain) {
-            case Chain.ETHERS: {
-                defineReadOnly(this, 'detectChainId', Api.detectChainId(this.send.bind(this)));
-                defineReadOnly(this, 'prepareRequest', Api.prepareRequest);
-                break;
-            }
-            case Chain.FISCO: {
-                defineReadOnly(this, 'detectChainId', Api$1.detectChainId(this.send.bind(this)));
-                defineReadOnly(this, 'prepareRequest', Api$1.prepareRequest(this.groupId));
-                break;
-            }
-        }
+        defineReadOnly(this, 'getChainId', getStatic((new.target), 'getChainId')(chain, this.send.bind(this)));
+        defineReadOnly(this, 'prepareRequest', getStatic((new.target), 'prepareRequest')(chain, this.network, this.groupId));
         this._nextId = 42;
-    }
-    static defaultUrl() {
-        return defaultUrl;
-    }
-    static defaultNetwork() {
-        return Promise.resolve(defaultNetwork);
     }
     static getFormatter() {
         if (defaultFormatter$1 == null) {
@@ -20370,14 +20213,117 @@ class JsonRpcProvider extends BaseProvider {
         }
         return defaultFormatter$1;
     }
+    static defaultUrl() {
+        return defaultUrl;
+    }
+    static defaultNetwork() {
+        return Promise.resolve(defaultNetwork);
+    }
     static getNetwork(network) {
         return getNetwork((network == null) ? defaultNetwork : network);
+    }
+    static getChainId(chain, send) {
+        switch (chain) {
+            case Chain.ETHERS:
+                return () => send('eth_chainId', []);
+            case Chain.FISCO:
+                return () => send('getClientVersion', []).then((clientVersion) => Number(clientVersion['Chain Id']));
+        }
+        return logger$t.throwArgumentError('invalid chain', 'chain', chain);
+    }
+    static prepareRequest(chain, _, groupId) {
+        switch (chain) {
+            case Chain.ETHERS:
+                return (method, params) => {
+                    switch (method) {
+                        case 'getBlockNumber':
+                            return ['eth_blockNumber', []];
+                        case 'getGasPrice':
+                            return ['eth_gasPrice', []];
+                        case 'getBalance':
+                            return ['eth_getBalance', [params.address.toLowerCase(), params.blockTag]];
+                        case 'getTransactionCount':
+                            return ['eth_getTransactionCount', [params.address.toLowerCase(), params.blockTag]];
+                        case 'getCode':
+                            return ['eth_getCode', [params.address.toLowerCase(), params.blockTag]];
+                        case 'getStorageAt':
+                            return ['eth_getStorageAt', [params.address.toLowerCase(), params.position, params.blockTag]];
+                        case 'sendTransaction':
+                            return ['eth_sendRawTransaction', [params.signedTransaction]];
+                        case 'getBlock':
+                            if (params.blockTag) {
+                                return ['eth_getBlockByNumber', [params.blockTag, !!params.includeTransactions]];
+                            }
+                            else if (params.blockHash) {
+                                return ['eth_getBlockByHash', [params.blockHash, !!params.includeTransactions]];
+                            }
+                            return null;
+                        case 'getTransaction':
+                            return ['eth_getTransactionByHash', [params.transactionHash]];
+                        case 'getTransactionReceipt':
+                            return ['eth_getTransactionReceipt', [params.transactionHash]];
+                        case 'call': {
+                            return ['eth_call', [params.transaction, params.blockTag]];
+                        }
+                        case 'estimateGas': {
+                            return ['eth_estimateGas', [params.transaction]];
+                        }
+                        case 'getLogs':
+                            if (params.filter && params.filter.address != null) {
+                                params.filter.address = params.filter.address.toLowerCase();
+                            }
+                            return ['eth_getLogs', [params.filter]];
+                    }
+                    return null;
+                };
+            case Chain.FISCO:
+                return (method, params) => {
+                    switch (method) {
+                        case 'getClientVersion':
+                            return ['getClientVersion', []];
+                        case 'getPbftView':
+                            return ['getPbftView', [groupId]];
+                        case 'getSealerList':
+                            return ['getSealerList', [groupId]];
+                        case 'getObserverList':
+                            return ['getObserverList', [groupId]];
+                        case 'getSyncStatus':
+                            return ['getSyncStatus', [groupId]];
+                        case 'getPeers':
+                            return ['getPeers', [groupId]];
+                        case 'getNodeIdList':
+                            return ['getNodeIDList', [groupId]];
+                        case 'getGroupList':
+                            return ['getGroupList', [groupId]];
+                        case 'getBlockNumber':
+                            return ['getBlockNumber', [groupId]];
+                        case 'getBlock':
+                            if (params.blockTag) {
+                                return ['getBlockByNumber', [groupId, params.blockTag, !!params.includeTransactions]];
+                            }
+                            else if (params.blockHash) {
+                                return ['getBlockByHash', [groupId, params.blockHash, !!params.includeTransactions]];
+                            }
+                            break;
+                        case 'sendTransaction':
+                            return ['sendRawTransaction', [groupId, params.signedTransaction]];
+                        case 'getTransaction':
+                            return ['getTransactionByHash', [groupId, params.transactionHash]];
+                        case 'getTransactionReceipt':
+                            return ['getTransactionReceipt', [groupId, params.transactionHash]];
+                        case 'call':
+                            return ['call', [groupId, params.transaction]];
+                    }
+                    return null;
+                };
+        }
+        return logger$t.throwArgumentError('invalid chain', 'chain', chain);
     }
     detectNetwork() {
         return __awaiter$4(this, void 0, void 0, function* () {
             let network = this.network;
             try {
-                const chainId = yield this.detectChainId();
+                const chainId = yield this.getChainId();
                 if (chainId) {
                     network.chainId = Number(chainId);
                 }
@@ -20396,7 +20342,7 @@ class JsonRpcProvider extends BaseProvider {
             }
         });
     }
-    getResult(payload) {
+    result(payload) {
         return payload.result;
     }
     send(method, params) {
@@ -20412,7 +20358,7 @@ class JsonRpcProvider extends BaseProvider {
                 request: deepCopy(request),
                 provider: this,
             });
-            return fetchJson(this.connection, JSON.stringify(request), this.getResult).then((result) => {
+            return fetchJson(this.connection, JSON.stringify(request), this.result).then((result) => {
                 this.emit('debug', {
                     action: 'response',
                     request: request,
